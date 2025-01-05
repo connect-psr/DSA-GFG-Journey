6db182190cf6cbf6660cdf8b8edf0bd3e65b134e
rajput_prathameshsigh <rprathameshsingh@gmail.com>
2025-01-05 10:16:38 +0530

    Update Print LinkedList

--- Basic/PrintLinkedList.java
+++ Basic/PrintLinkedList.java
@@ -1,21 +1,15 @@
-/* Node is defined as
-class Node {
+class Node{
     int data;
     Node next;
-    Node(int x) {
+
+    Node(int x){
         data = x;
         next = null;
     }
 }
-*/
 
-/*
-    Print elements of a linked list on console
-    Head pointer input could be NULL as well for empty list
-*/
-
-class Solution {
-    void display(Node head) {
+class Solution{
+    void display(Node head){
         while(head != null){
             System.out.print(head.data+" "); // Print current node's data
             head = head.next; // Move to the next node
@@ -23,8 +17,13 @@ class Solution {
     }
 }
 
-public class PrintLinkedList {
-    public static void main(String[] args) {
-        // Test the solution
+public class PrintLinkedList{
+    public static void main(String[] args){
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        
+        new Solution().display(head); // Output: 1 2 3
     }
 }
+
